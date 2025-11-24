4096cdd6bc6fe282044f80ea1a7241b33fbcc9d3
squidbob67 <s3pattranite@gmail.com>
2025-11-24 12:40:50 +0200

    Add files via upload

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniProjector.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
